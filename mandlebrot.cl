fab69fe472f6c7c9fb7c99ce51d07e7935860f15
Henry Bradlow <hbradlow@berkeley.edu>
2014-06-13 16:16:05 -0400

    Added better zooming with box

--- mandlebrot.cl
+++ mandlebrot.cl
@@ -1,21 +1,26 @@
-__kernel void mandlebrot(__global float* width, 
-                                  __global float* height, 
+__kernel void mandlebrot(__global float* params, 
                                   __global float* c)
 {
+    float width = params[0];
+    float height = params[1];
+    float x0 = params[2];
+    float y0= params[3];
+    float x1 = params[4];
+    float y1= params[5];
+
     unsigned int index = get_global_id(0);
 
     unsigned int num_iterations = 100;
 
-    unsigned int i = index%((int)width[0]);
-    unsigned int j = index/((int)width[0]);
+    unsigned int i = index%(int)width;
+    unsigned int j = index/(int)height;
     
-    float x = i*4.f/width[0]-2;
-    float y = j*4.f/height[0]-2;
+    float x = i * (float)(x1-x0)/width + x0;
+    float y = j * (float)(y1-y0)/height + y0;
 
     float zx = 0;
     float zy = 0;
     float xtemp = 0;
-
 
     for(int iterations = 0; iterations < num_iterations; iterations ++){
         if(zx*zx + zy*zy > 2*2){
--- mandlebrot_cl.py
+++ mandlebrot_cl.py
@@ -1,8 +1,11 @@
 import pyopencl as cl
 import numpy
-from matplotlib import pyplot as plt
 import time
 import Tkinter
+from PIL import Image, ImageTk
+
+from matplotlib import pyplot as plt
+from matplotlib.patches import Rectangle
 
 class CL:
     def __init__(self):
@@ -12,48 +15,100 @@ class CL:
         self.w = 1000
         self.h = 1000
 
+        self.pressed = False
+        self.rectangle = None
+
+        self.x0 = -2
+        self.y0 = -2
+        self.x1 = 2
+        self.y1 = 2
+
+        self.fig = None
+
     def loadProgram(self,filename):
         f = open(filename, 'r')
         fstr = "".join(f.readlines())
         self.program = cl.Program(self.ctx, fstr).build()
-    def popCorn(self):
+    def run(self,x0,y0,x1,y1):
         mf = cl.mem_flags
 
         #initialize client side (CPU) arrays
-        self.width = numpy.array([self.w], dtype=numpy.float32)
-        self.height = numpy.array([self.h], dtype=numpy.float32)
+        self.params = numpy.array([self.w,self.h,x0,y0,x1,y1], dtype=numpy.float32)
         self.c = numpy.zeros([self.h*self.w],dtype=numpy.float32)
 
         #create OpenCL buffers
-        self.width_buf = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, 
-                                                            hostbuf=self.width)
-        self.height_buf = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, 
-                                                            hostbuf=self.height)
+        self.params_buf = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, 
+                                                            hostbuf=self.params)
+        print self.params
         self.dest_buf = cl.Buffer(self.ctx, mf.WRITE_ONLY, 
                                                             self.c.nbytes)
-    def execute(self):
+
+
         self.program.mandlebrot(self.queue, self.c.shape, None, 
-                                    self.width_buf, self.height_buf, self.dest_buf)
+                                    self.params_buf, self.dest_buf)
         cl.enqueue_read_buffer(self.queue, self.dest_buf, self.c).wait()
 
         return self.c.reshape(self.w,self.h)
+    def on_release(self,event):
+        self.release_x = event.xdata
+
+        self.release_y = self.press_y + (self.release_x-self.press_x)
+        #self.release_y = event.ydata
+
+        self.tx0 = self.press_x * float(self.x1-self.x0)/self.w + self.x0
+        self.ty0 = self.press_y * float(self.y1-self.y0)/self.h + self.y0
+        self.tx1 = self.release_x * float(self.x1-self.x0)/self.w + self.x0
+        self.ty1 = self.release_y * float(self.y1-self.y0)/self.h + self.y0
+
+        self.x0 = self.tx0
+        self.y0 = self.ty0
+        self.x1 = self.tx1
+        self.y1 = self.ty1
+
+        self.pressed = False
+
+        self.render()
+    def on_press(self,event):
+        self.press_x = event.xdata
+        self.press_y = event.ydata
+
+        self.pressed = True
+
+    def clear_rectangle(self):
+        if self.rectangle is not None:
+            self.rectangle.remove()
+            self.rectangle = None
+
+    def on_move(self,event):
+        if self.pressed:
+            self.clear_rectangle()
+
+            self.rectangle = self.ax.add_patch(Rectangle(
+                                            (self.press_x,self.press_y), 
+                                            event.xdata-self.press_x,
+                                            event.xdata-self.press_x,
+                                            facecolor="none"))
+            self.fig.canvas.draw()
+    def render(self):
+        self.clear_rectangle()
+
+        res = m.run(self.x0,self.y0,self.x1,self.y1)
+
+        self.ax.imshow(res, interpolation='nearest')
+        self.fig.canvas.draw()
+
+    def prepare_plot(self):
+        self.fig, self.ax = plt.subplots()
+
+        cid = self.fig.canvas.mpl_connect('button_press_event', m.on_press)
+        cid = self.fig.canvas.mpl_connect('button_release_event', m.on_release)
+        cid = self.fig.canvas.mpl_connect('motion_notify_event', m.on_move)
+
+        self.render()
+
+        plt.show()
 
 m = CL()
 m.loadProgram("mandlebrot.cl")
-m.popCorn()
 
-begin = time.time()
-res = m.execute()
-print "Total time: " + str(time.time()-begin)
-
-fig, ax = plt.subplots()
-ax.imshow(res, interpolation='nearest')
-
-def onclick(event):
-    print 'button=%d, x=%d, y=%d, xdata=%f, ydata=%f'%(
-        event.button, event.x, event.y, event.xdata, event.ydata)
-
-cid = fig.canvas.mpl_connect('button_press_event', onclick)
-cid = fig.canvas.mpl_connect('button_release_event', onclick)
-
-plt.show()
+m.prepare_plot()
